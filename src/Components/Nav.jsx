7d1e175d6028395f1d5a577cb3e83d5ff2e4af79
l1f21bsse0424 <l1f21bsse0424@ucp.edu.pk>
2025-02-27 14:53:03 +0500

    done 1

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='bg-slate-950 flex justify-center w-full h-10'>
+        <h1 className='className=" text-2xl font-bold text-white'>Kanban</h1>
+    </div>
+  )
+}
+
+export default Nav
